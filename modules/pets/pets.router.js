61aaec26b639b879f8cccb1676557bae03422556
John Bowser <john.bow7720@gmail.com>
2020-10-02 09:18:21 -0600

    set up pets router

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -8,10 +8,16 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   // Return all pets currently up for adoption.
+  const pets = Pets.get()
+  res.status(200).json(pets)
 })
 
 router.delete('/', json, (req, res) => {
   // Remove a pet from adoption.
+  // cats of dogs
+  const { pet } = req.body
+  Pets.dequeue(pet)
+  res.status(204)
 })
 
 module.exports = router
