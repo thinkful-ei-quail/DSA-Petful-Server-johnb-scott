d20957c26431e421f2a5eefffd74babce1fa112a
John Bowser <john.bow7720@gmail.com>
2020-10-02 09:25:29 -0600

    set up people router

--- modules/people/people.router.js
+++ modules/people/people.router.js
@@ -7,10 +7,15 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   // Return all the people currently in the queue.
+  const people = People.get()
+  res.status(200).send(people) //check to make sure this works
 })
 
 router.post('/', json, (req, res) => {
   // Add a new person to the queue.
+  const { name } = req.body
+  People.enqueue(name)
+  res.status(201)
 })
 
 module.exports = router
