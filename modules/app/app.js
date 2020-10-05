5acf0f41ab12b2e746339170ea13447600058dfa
John Bowser <john.bow7720@gmail.com>
2020-10-05 17:21:43 -0600

    troubleshooting cors bug

--- modules/app/app.js
+++ modules/app/app.js
@@ -3,10 +3,7 @@ const cors = require('cors')
 const CLIENT_ORIGIN = require('../../config')
 const app = express()
 
-app.use(cors({
-  origin: CLIENT_ORIGIN
-})
-)
+app.use(cors())
 
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
